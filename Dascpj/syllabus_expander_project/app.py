b53183437df28350615f6ece196faa2ab235dd8f
SaeeD10101 <107804241+SaeeD10101@users.noreply.github.com>
2025-12-20 21:51:07 +0300

    Update app.py

--- Dascpj/syllabus_expander_project/app.py
+++ Dascpj/syllabus_expander_project/app.py
@@ -9,10 +9,10 @@ sys.path.insert(0, 'src')
 from src.topic_extractor import TopicExtractor
 from src.module_structurer import ModuleStructurer
 from src.outcome_generator import OutcomeGenerator
-from assessment_generator import AssessmentBlueprintGenerator
-from question_generator import QuestionGenerator
-from alignment_matrix import AlignmentMatrixGenerator
-from assessment_analyzer import AssessmentAnalyzer
+from src.assessment_generator import AssessmentBlueprintGenerator
+from src.question_generator import QuestionGenerator
+from src.alignment_matrix import AlignmentMatrixGenerator
+from src.assessment_analyzer import AssessmentAnalyzer
 
 app = Flask(__name__, static_folder='web', static_url_path='')
 CORS(app)
@@ -142,3 +142,4 @@ if __name__ == '__main__':
     
 
     app.run(debug=True, host='0.0.0.0', port=5000)
+
